2bc1505750d27451c7a09c0258d7a323edfc1b87
hringdal <hakon.ringdal@gmail.com>
2017-03-22 10:33:01 +0100

    Fix errors

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -9,9 +9,6 @@ Books.attachSchema(Schemas.Book);
 export const Courses = new Mongo.Collection('courses');
 Courses.attachSchema(Schemas.Course);
 
-export const Chapters = new Mongo.Collection('chapters');
-Chapters.attachSchema(Schemas.Chapter);
-
 export const Results = new Mongo.Collection('results');
 Results.attachSchema(Schemas.Result);
 
