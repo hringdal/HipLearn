3a69a2769b15b97227b8b267df8486d8f073cbd8
hringdal <hakon.ringdal@gmail.com>
2017-03-22 09:46:57 +0100

    for master

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,26 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-<<<<<<< HEAD
 
-// Layout and all route targets
-// Each page imports its own components and other pages that it references
-import '../../ui/layouts/app-body.js';
-import '../../ui/pages/list.js';
-import '../../ui/pages/placeholder.js';
-import '../../ui/pages/app-not-found.js';
-import '../../ui/pages/tasks.js';
-import '../../ui/pages/login_Student.js';
-
-// route to login
-FlowRouter.route('/login', {
-    name: 'login_Student.show',
-    action() {
-        BlazeLayout.render('App_body', {main: 'login_Student'})
-    }
-});
-=======
-import { Books } from '../../api/collections.js';
->>>>>>> a35b4ffefd903544fe03af57b876d6a410272925
 // Routes
 FlowRouter.route('/', {
   name: 'App.home',
@@ -34,12 +14,6 @@ FlowRouter.route('/list', {
   action() {
     BlazeLayout.render('App_body', { main: 'List' });
   },
-});
-FlowRouter.route('/janne', {
-    name: 'Tasks.show',
-    action() {
-        BlazeLayout.render('App_body', { main: 'tasks' });
-    },
 });
 
 FlowRouter.route('/form/', {
